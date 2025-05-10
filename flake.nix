e384938e6853df190eff43b51ee3a8e27773c16d
Paulemeister <annanas6800i@gmail.com>
2025-05-10 02:17:27 +0200

    initial commit

---
+++ flake.nix
@@ -0,0 +1,45 @@
+{
+  description = "Your new nix config";
+
+  inputs = {
+    # Nixpkgs
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
+
+    # Home manager
+    home-manager = {
+      url = "github:nix-community/home-manager/release-24.11";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    home-manager,
+    ...
+  } @ inputs: let
+    inherit (self) outputs;
+  in {
+    # NixOS configuration entrypoint
+    # Available through 'nixos-rebuild --flake .#your-hostname'
+    nixosConfigurations = {
+      
+     nixos = nixpkgs.lib.nixosSystem {
+        specialArgs = {inherit inputs outputs;};
+        # > Our main nixos configuration file <
+        modules = [
+          ./nixos/configuration.nix
+        
+          home-manager.nixosModules.home-manager {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+
+            home-manager.users.paulemeister = import ./home-manager/home.nix;
+
+            # Optionally, use home-manager.extraSpecialArgs to pass arguments to home.nix
+          }
+        ];
+      };
+    };
+  };
+}
---
+++ home-manager/home.nix
@@ -0,0 +1,73 @@
+# This is your home-manager configuration file
+# Use this to configure your home environment (it replaces ~/.config/nixpkgs/home.nix)
+{
+  inputs,
+  lib,
+  config,
+  pkgs,
+  ...
+}: {
+  # You can import other home-manager modules here
+  imports = [
+    # If you want to use home-manager modules from other flakes (such as nix-colors):
+    # inputs.nix-colors.homeManagerModule
+
+    # You can also split up your configuration and import pieces of it here:
+    # ./nvim.nix
+  ];
+
+  
+  home = {
+    username = "paulemeister";
+    homeDirectory = "/home/paulemeister";
+  };
+
+  # Add stuff for your user as you see fit:
+  # programs.neovim.enable = true;
+  home.packages = with pkgs;[
+    steam
+    nixd # nix language server
+    alejandra # nix formatter
+  ];
+
+  programs = {
+      
+    helix = {
+      enable = true;
+      settings = {
+        editor.line-number = "relative";
+      };
+      languages.language-server.nixd = {
+        formatting.command = ["alejandra"];
+      };
+    };
+  
+    # Enable home-manager and git
+    home-manager.enable = true;
+    git = {
+      enable = true;
+      userEmail = "annanas6800i@gmail.com";
+      userName = "Paulemeister";
+    };
+    bash.enable = true;
+  };
+  # Nicely reload system units when changing configs
+  systemd.user.startServices = "sd-switch";
+
+  dconf = {
+    enable = true;
+    settings = {
+#      "org/gnome/desktop/input-sources" = {
+#        show-all-sources = true;
+#        sources = [ (lib.gvariant.mkTuple [ "xkb" "eu" ]) ];
+#        xkb-options = [ ];
+#      };
+      "org/gnome/desktop/interface" = {
+      color-scheme = "prefer-dark";
+      };
+    };
+  };
+  
+  # https://nixos.wiki/wiki/FAQ/When_do_I_update_stateVersion
+  home.stateVersion = "23.05";
+}
