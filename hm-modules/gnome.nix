05e44ee1033ad10d0e7f82e65bae68585c16963f
Paulemeister <annanas6800i@gmail.com>
2026-03-23 00:22:05 +0100

    use gvariant for pop-shell values-> update on rebuild works

--- hm-modules/gnome.nix
+++ hm-modules/gnome.nix
@@ -243,8 +243,8 @@ in
           };
           # Configure pop-shell
           "org/gnome/shell/extensions/pop-shell" = {
-            gap-inner = lib.mkDefault osCfg.de.gnome.popBorderSize;
-            gap-outer = lib.mkDefault osCfg.de.gnome.popBorderSize;
+            gap-inner = lib.mkDefault (lib.hm.gvariant.mkUint32 osCfg.de.gnome.popBorderSize);
+            gap-outer = lib.mkDefault (lib.hm.gvariant.mkUint32 osCfg.de.gnome.popBorderSize);
             tile-by-default = lib.mkDefault true;
           };
           # Configure vertical workspaces
