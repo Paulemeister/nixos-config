39952ad3aa30ff68b1b3dffe1bf928b143e420b1
Paulemeister <annanas6800i@gmail.com>
2026-04-20 16:42:10 +0200

    added gnu octave, replaced legacy pkgs.system

--- hm-modules/simulation-tools.nix
+++ hm-modules/simulation-tools.nix
@@ -2,12 +2,14 @@
   lib,
   inputs,
   osConfig,
+  config,
   pkgs,
   ...
 }:
 let
   osCfg = osConfig.pm-modules;
-  inherit (lib) mkIf;
+  cfg = config.pm-modules;
+  inherit (lib) mkIf mkMerge;
 
   paraview-5-13 =
     pkgs.runCommand "paraview-5-13"
@@ -21,13 +23,13 @@ let
         # 1. Create a symlink for the main binary with the new name
         # This points directly to the real binary in the original store path
         ln -s ${
-          inputs.nixpkgs-paraview-5-13-2.legacyPackages.${pkgs.system}.paraview
+          inputs.nixpkgs-paraview-5-13-2.legacyPackages.${pkgs.stdenv.hostPlatform.system}.paraview
         }/bin/paraview $out/bin/paraview-5-13
 
         # 2. Copy and edit the desktop file
         # We find the desktop file in the original package
         DESKTOP_SRC="${
-          inputs.nixpkgs-paraview-5-13-2.legacyPackages.${pkgs.system}.paraview
+          inputs.nixpkgs-paraview-5-13-2.legacyPackages.${pkgs.stdenv.hostPlatform.system}.paraview
         }/share/applications/org.paraview.ParaView.desktop"
 
         cp "$DESKTOP_SRC" $out/share/applications/paraview-5-13.desktop
@@ -39,10 +41,19 @@ let
       '';
 in
 {
-  config = mkIf osCfg.simulation-tools.enable {
-    home.packages = [
-      paraview-5-13
-      pkgs.paraview
-    ];
-  };
+  config = mkIf osCfg.simulation-tools.enable (mkMerge [
+    {
+      home.packages = with pkgs; [
+        paraview-5-13
+        paraview
+        octaveFull
+      ];
+    }
+    (mkIf cfg.usePersistence {
+      home.persistence."/persist".directories = [
+        ".config/octave"
+        ".local/share/octave"
+      ];
+    })
+  ]);
 }
