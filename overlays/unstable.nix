d5de83c3310f92255c12b23e01fb2849ecdf46d7
Paulemeister <annanas6800i@gmail.com>
2026-05-27 21:49:33 +0200

    remove warning for system rename

--- overlays/unstable.nix
+++ overlays/unstable.nix
@@ -1,7 +1,7 @@
 inputs:
 (final: prev: {
   unstable = import inputs.nixpkgs-unstable {
-    system = prev.system;
+    system = prev.stdenv.hostPlatform.system;
     config.allowUnfree = true;
   };
 })
